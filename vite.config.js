96f8760075162d00ca3d2a3d76ef4c2f94352c3c
lsantonicola7-ux <lsantonicola7@gmail.com>
2026-05-24 16:32:26 +0200

    Remove server proxy configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // In dev, proxy API calls to a local express or just skip
-      '/.netlify': 'http://localhost:3001',
-    },
+  build: {
+    target: 'es2015',
   },
 })
